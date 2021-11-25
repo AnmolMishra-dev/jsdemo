62b5339445ae0e069780cb268af317710006fc1e
Anmol Mishra <tbsdev3@gmail.com>
2021-11-25 12:24:12 +0530

    Demo

---
+++ jsPage.js
@@ -0,0 +1,12 @@
+console.log("Hello");
+console.log(34+5);
+const array=[1,2,3,4,5,6,7,8,9]
+array.push(34);
+console.log(array);
+var Name='jon';
+{
+let   Name ="Rahul";
+console.log(Name)
+
+}
+console.log(Name);
