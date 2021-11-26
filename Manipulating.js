0a6380784251b0bfceb21895b36978c0200aa768
Anmol Mishra <tbsdev3@gmail.com>
2021-11-26 16:56:46 +0530

    Demo

---
+++ Manipulating.js
@@ -0,0 +1,8 @@
+console.log("Manipulating");
+let a=window;
+console.log(a);
+alert("Hola, you have successfully");
+b=prompt("Hola,please enter");
+console.log(b); 
+c=confirm("Please enter");
+console.log(c);
