b41b9ca27d10eda7a166cba1ef842a7997ab71fa
Anmol Mishra <tbsdev3@gmail.com>
2021-11-29 14:42:07 +0530

    Demo

--- element.js
+++ element.js
@@ -3,4 +3,16 @@ let element = document.getElementById("first");
 element.style.color="red";
 element.style.background="black"
 element.innerText="Hello"
-console.log(element);
+//SINGLE LINE  SELECTEES
+let sel=document.querySelector("#first");
+sel=document.querySelector(".child");
+sel=document.querySelector("div");
+sel.style.color="greEn"
+
+console.log(sel);
+//MULTILINE SELECTEES
+
+let ele=document.getElementsByClassName("child");
+ele=document.getElementsByClassName("container");
+ele=document.getElementsByTagName('div');
+console.log('ele');
