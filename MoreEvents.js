85594235cde25889f6f7dd766fc34cf22946680d
Anmol Mishra <tbsdev3@gmail.com>
2021-12-06 17:26:06 +0530

    DEMO

--- MoreEvents.js
+++ MoreEvents.js
@@ -1,10 +1,21 @@
 console.log("More Events");
 let btn= document.getElementById('btn');
 
-btn.addEventListener('click', func1);
+btn.addEventListener('click',func1);
+btn.addEventListener('dblclick',func2);
 
 function func1(e) {
 console.log("Thanks", e);
 e.preventDefault();
 
 } 
+function func2(e) {
+    console.log("Thanks dblclick" , e);
+    e.preventDefault();
+    
+    } 
+
+    document.querySelector('.container').addEventListener('mousemove',function(e){
+document.body.style.backgroundColor=`rgb(${e.offsetX},${e.offsetX},150)`;
+console.log("you triggered mouse move event")
+    });
