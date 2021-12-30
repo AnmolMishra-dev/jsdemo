25f7a0150a519475acf064c2bc2095c470562ec1
Anmol Mishra <tbsdev3@gmail.com>
2021-12-30 15:14:20 +0530

    Alarm

---
+++ projectjs/Alarm Clock/index.js
@@ -0,0 +1,36 @@
+console.log("This Alarm");
+const alarmSubmit = document.getElementById('alarmSubmit');
+var toastLiveExample = document.getElementById('liveToast');
+
+// Add an event listener to the submit button
+alarmSubmit.addEventListener('click', setAlarm);
+
+var audio = new Audio('https://www.soundjay.com/phone/sounds/telephone-ring-01a.mp3');
+
+// function to play the alarm ring tone
+function ringBell() {
+     audio.play();
+}
+
+// This function will run whenever alarm is set from the UI
+function setAlarm(e) {
+    e.preventDefault();
+    const alarm = document.getElementById('alarm');
+    alarmDate = new Date(alarm.value);
+    console.log(`Setting Alarm for ${alarmDate}...`);
+    now = new Date();
+
+    let timeToAlarm = alarmDate - now;
+    console.log(timeToAlarm);
+    if(timeToAlarm>=0){
+        setTimeout(() => {
+            console.log("Ringing now")
+            var toast = new bootstrap.Toast(toastLiveExample)
+
+            toast.show('autohide');
+          
+            ringBell();
+        }, timeToAlarm);
+    }
+}
+
